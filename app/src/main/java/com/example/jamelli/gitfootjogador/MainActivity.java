70b8bce7a0d00954a4beb69c2d2fe331d8d48886
brunojamelli <brunojamelli@ufrn.edu.br>
2018-11-20 20:47:58 -0300

    começando o projeto e criando o navigation drawer

---
+++ app/src/main/java/com/example/jamelli/gitfootjogador/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.jamelli.gitfootjogador;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void entrarMenu(View v){
+        Intent i = new Intent(getApplicationContext(),DrawerActivity.class);
+        startActivity(i);
+    }
+}
---
+++ app/src/main/java/com/example/jamelli/gitfootjogador/fragment/FragmentCadastro.java
@@ -0,0 +1,6 @@
+package com.example.jamelli.gitfootjogador.fragment;
+
+import android.support.v4.app.Fragment;
+
+public class FragmentCadastro extends Fragment{
+}
